37b90e93961939d7bba4a01a5d2e007c942a8032
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-03-08 11:24:26 +0100

    first commit

---
+++ src/main/java/de/neuefische/demo/GokuController.java
@@ -0,0 +1,21 @@
+package de.neuefische.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/blast")
+public class GokuController {
+    
+@GetMapping("/{name}")
+public String blast(@PathVariable String name) {
+
+return name + "KAAAAMEEEE " + "HAAAMEEEE" + "HAAAAAAA!!!!"; 
+
+}
+
+}
+
+
